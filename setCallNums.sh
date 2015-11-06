992d7d327797fccb2410a0a0a66030b01de74126
Tucker James Leavitt <tuckerl@myth6.Stanford.EDU>
2015-11-06 11:09:44 -0800

    Obfuscated numbers

--- setCallNums.sh
+++ setCallNums.sh
@@ -1,12 +1,5 @@
 #Usage: . setCallNums.sh
 
-Numbers[0]=9703171291 #ROLLIN
-Numbers[1]=9703178607 #CLARA
-Numbers[2]=3604313565 #GRANT
-Numbers[3]=8018347702 #JAKE
-Numbers[4]=6509245563 #PRITHVI
-Numbers[5]=9736995095 #NATHANIEL
-Numbers[7]=9707495574 #RICHARD
-Numbers[8]=9707597536 #HOLLY
-Numbers[9]=9707791536 #ME
-# Numbers[1]=0 #RANDO, will probably fail
+Numbers[0]=1234567890 #FIRST NUMBER
+Numbers[1]=1235551234 #SECOND NUMBER
+# ...
